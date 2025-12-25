6ef3ed2c90ebce5085e99b58e4eac4ca7be172e5
adithn04 <adithyan1912@gmail.com>
2025-12-25 21:35:25 +0530

    leetcode

---
+++ #how many numbers are smaller than the c.py
@@ -0,0 +1,20 @@
+# how many numbers are smaller than the current number
+nums=[8,1,2,2,3]
+count = 0
+new=[]
+for i in range(len(nums)):
+    for j in range(len(nums)):
+        if nums[i]>nums[j]:
+            count+=1
+    new.append(count)
+    count = 0
+print(new)
+# nums = [8, 1, 2, 2, 3]
+# result = []
+# for i in range(len(nums)):
+#     count = 0
+#     for j in range(len(nums)):
+#         if nums[j] < nums[i]:
+#             count += 1
+#     result.append(count)
+# print(result)
