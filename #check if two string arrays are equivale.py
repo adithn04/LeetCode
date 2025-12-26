efd55e935d199c2f416555ab4226d183a3cbb062
adithn04 <adithyan1912@gmail.com>
2025-12-26 22:16:33 +0530

    Leetcode

---
+++ #check if two string arrays are equivale.py
@@ -0,0 +1,14 @@
+#check if two string arrays are equivalent
+word1=["ab","c"]
+word2=["a","bc"]
+a=""
+b=""
+for i in range(len(word1)):
+    a+=word1[i]
+for i in range(len(word2)):
+    b+=word2[i]
+if a==b:
+    print(True)
+else:
+    print(False)
+
--- #how many numbers are smaller than the c.py
+++ #how many numbers are smaller than the c.py
@@ -9,12 +9,3 @@ for i in range(len(nums)):
     new.append(count)
     count = 0
 print(new)
-# nums = [8, 1, 2, 2, 3]
-# result = []
-# for i in range(len(nums)):
-#     count = 0
-#     for j in range(len(nums)):
-#         if nums[j] < nums[i]:
-#             count += 1
-#     result.append(count)
-# print(result)
